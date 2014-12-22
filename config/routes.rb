eb0db0590999464be38143752707827e3e94eaa4
The Cake Boss <cakeboss@undergroundpowers.com>
2014-12-22 02:00:24 +0000

    Added a pages controller and updated the routes.

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  root 'pages#home'
+  
+  get 'pages/home' => 'pages#home'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
